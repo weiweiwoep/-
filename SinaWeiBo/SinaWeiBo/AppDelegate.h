7c10ffef9419129e73131ee9c796dbbbdd3ec14d
weiwei <weiweiwoep@163.com>
2015-04-23 16:07:12 +0800

    创建项目

---
+++ SinaWeiBo/SinaWeiBo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SinaWeiBo
+//
+//  Created by 魏伟 on 15/4/23.
+//  Copyright (c) 2015年 weiweiwoep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
